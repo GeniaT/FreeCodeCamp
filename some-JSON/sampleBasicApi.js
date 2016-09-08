d454602c7012ad7ed936caaca30d5424b2c47f03
Genia <tchistiakov.e@gmail.com>
2016-09-08 23:32:32 +0200

    link between youtube example and API available
    html updated to execute the file

--- some-JSON/sampleBasicApi.js
+++ some-JSON/sampleBasicApi.js
@@ -1,8 +1,33 @@
+/* notes:
 $.ajax({
   type: 'GET',
-  url: 'http://rest.learncode.academy/api/johnbob/friends', //That's where we will take (GET) the data from
+  url: 'http://rest.learncode.academy/api/johnbob/friends',
   success: function(data) {
-    console.log("I have friends!", data); //returns all of johnbob's friends //Raw data is displayed in the console. 
+    console.log("I have friends!", data); //returns all of johnbob's friends
   }
+});
+
+$.ajax({
+  type: 'POST',
+  url: 'http://rest.learncode.academy/api/johnbob/friends',
+  data: {name: 'Billy Bob', age: 27},
+  success: function(data) {
+    console.log("Friend added!", data); //the new item is returned with an ID
+  }
+});
+*/
+//==============================================================================
+//Lets adapte API of john friends to his orders example.
+$(function() {
+  var $friends = $('#friends');
+  $.ajax({
+    type:'GET',
+    url: 'http://rest.learncode.academy/api/johnbob/friends',
+    success: function (friends) {
+      $.each(friends, function(i, friend) {
+        $friends.append('<li>name: '+ friend.name +', age: ' +friend.age +'</li>');
+      });
+    }
+  });
 });
 
