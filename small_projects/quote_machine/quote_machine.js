ccc33ea8e636ce35088f10b8d638c83cbd952cef
Genia <tchistiakov.e@gmail.com>
2016-09-13 22:41:27 +0200

    commenting

--- small_projects/quote_machine/quote_machine.js
+++ small_projects/quote_machine/quote_machine.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
 
 
 
-
+// /!\ IMPORTANT!!!!! When selecting data from API, check if you go through an object or array:
+//Example : data.contents.quotes[0].quote (data is the root of the whole JSON call, contents is object and quotes is array.)
 
 //continue on https://www.smashingmagazine.com/2012/02/beginners-guide-jquery-based-json-api-clients/
 //There is shown how to get a specific data from API "complex" objects.
