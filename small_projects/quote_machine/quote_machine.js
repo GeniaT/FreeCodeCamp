9c6b7b12aa6886f66f7f6f7b67e918f5de7e5650
Genia <tchistiakov.e@gmail.com>
2016-09-13 23:45:28 +0200

    JSON and JSONP versions of quote API
    and comments for futur uses.

--- small_projects/quote_machine/quote_machine.js
+++ small_projects/quote_machine/quote_machine.js
@@ -1,17 +1,37 @@
+/* WITH JSON: No Cross-origin error but data from API is obsolete
 $(document).ready(function() {
   $('.newQuote').on('click', function () {
-    $.getJSON('http://quotes.rest/qod.json', function(data) {
-      console.log(data);                          //Testing the data from API.
-      console.log(data.contents.quotes[0].quote); //testing a specific part of the data. 
+    $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&callback=?', function(data) {
+      console.log(data);
+      console.log(data.quoteText + "- " + data.quoteAuthor);
+    });
+  });
+});
+*/
+
+//With JSONP but originally with Cross-Origin error even with "callback=?"
+$(document).ready(function() {
+  $('.newQuote').on('click', function() {
+    $.ajax ({
+      type: 'GET',
+      //url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&callback=?',
+      url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=jsonp_callback', //jsonp=jsonp_callback the API required to specify the callback function name
+      dataType: "jsonp",
+      jsonp: 'callback',                                                          //Here we define the callback function name that is now insterted in the URL
+      jsonpCallback: 'jsonp_callback',
+      success: function (data) {
+        console.log(data);                                                        //The raw data is now displayed and we can work with it.
+        console.log(data.quoteText + "- " + data.quoteAuthor);
+      }
     });
   });
 });
 
-
-
-
-// /!\ IMPORTANT!!!!! When selecting data from API, check if you go through an object or array:
+/* /!\ USEFULL GENERAL INFO:
+1/ When selecting data from API, check if you go through an object or array:
 //Example : data.contents.quotes[0].quote (data is the root of the whole JSON call, contents is object and quotes is array.)
-
-//continue on https://www.smashingmagazine.com/2012/02/beginners-guide-jquery-based-json-api-clients/
-//There is shown how to get a specific data from API "complex" objects.
+2/other point: to avoid Cross-origin errors: use &callback=? at the end of the url used for API call.
+3/The url build is important and never the same. It's specific to each API.
+4/ callback function "callback=?" can be enough in JSON for cross-origin errors but not in jsonp where we need to create a name for the callback
+function, then declare and use it. 
+*/
