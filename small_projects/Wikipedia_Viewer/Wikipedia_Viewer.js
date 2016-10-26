16f9e7fd1dfeebad96391166702189e7ae8aa50d
GeniaT <tchistiakov.e@gmail.com>
2016-10-26 23:37:56 +0200

    first search by user works
    Need to clear out the article divs before another search from the user.

--- small_projects/Wikipedia_Viewer/Wikipedia_Viewer.js
+++ small_projects/Wikipedia_Viewer/Wikipedia_Viewer.js
@@ -1,20 +1,13 @@
-/*
-var frm = document.getElementById('inp');
-var userInput = frm.value; //Empty at first, that's why it works after refresh
-var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22';
-var urlforAjax = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22' + userInput + '%22&format=json&callback=?%22';
-*/
+
+var userInput = $("#searchTerm").val(); //Allows to get the data from the input value set by the user
+var urlForAjax = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + userInput + '&format=json&callback=?';
 
 function getWiki() {
-  var frm = document.getElementById('inp');
-  var userInput = frm.value; //Empty at first, that's why it works after refresh
-  //var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22';
-  var urlForAjax = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22' + userInput + '%22&format=json&callback=?%22';
 
 $.ajax({
-  type: 'GET',
-  url: urlForAjax, //'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22',
-  dataType: 'jsonp',
+  type: "GET",
+  url: urlForAjax,
+  dataType: "jsonp",
   success: function (data) {
 
     for (var i = 0; i < 10; i++) {
@@ -30,26 +23,14 @@ $.ajax({
 
 
 
-$('.inp').on('click', function () {
-  //$('form').attr('action', urlForAjax); // Normally not mandatory if api call is done with correct url
+$(document).ready(function() {
+
+
+$('.inp').click(function () {
+  var userInput = $("#searchTerm").val(); //Allows to get the data from the input value set by the user
+  var urlForAjax = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + userInput + '&format=json&callback=?';
+
   getWiki();
 });
 
-/*
-function fromSearchBox () {
-  var frm = document.getElementById('inp');
-  var userInput = frm.value;
-  var urlForAjax;
-  if (frm) {
-    var urlForAjax=("https://en.wikipedia.org/w/api.php?action=opensearch&search=%22" + userInput + "%22&format=json&callback=?%22");
-    $('form').attr('action', frm.action); //now that the action link is known, we push it to html
-  }
-}
-
-$('.inp').on('click', function () {
-  var urlForAjax;
-  fromSearchBox();
 });
-*/
-
-getWiki();
