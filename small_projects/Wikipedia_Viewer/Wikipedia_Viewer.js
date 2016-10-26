9e56aa8e76af5b05c58ec8eeba36129c39286bef
Genia T <tchistiakov.e@gmail.com>
2016-10-26 17:47:02 +0200

    edit, no solution yet

--- small_projects/Wikipedia_Viewer/Wikipedia_Viewer.js
+++ small_projects/Wikipedia_Viewer/Wikipedia_Viewer.js
@@ -1,21 +1,41 @@
+/*
+var frm = document.getElementById('inp');
+var userInput = frm.value; //Empty at first, that's why it works after refresh
+var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22';
+var urlforAjax = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22' + userInput + '%22&format=json&callback=?%22';
+*/
+
 function getWiki() {
+  var frm = document.getElementById('inp');
+  var userInput = frm.value; //Empty at first, that's why it works after refresh
+  //var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22';
+  var urlForAjax = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22' + userInput + '%22&format=json&callback=?%22';
+
 $.ajax({
   type: 'GET',
-  url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22',
+  url: urlForAjax, //'https://en.wikipedia.org/w/api.php?action=opensearch&search=%22tennis%22&format=json&callback=?%22',
   dataType: 'jsonp',
   success: function (data) {
 
     for (var i = 0; i < 10; i++) {
-
       var url = data[3][i];
       var title = data[1][i];
       var description = data[2][i];
       $(".10articles").append("<div class='article'><a href=" + url + " target='_blank'>" + title + "</br>" + "<div class='description'>" + description + "</div>" + "</a></div>");
     }
+
   }
 });
 };
 
+
+
+$('.inp').on('click', function () {
+  //$('form').attr('action', urlForAjax); // Normally not mandatory if api call is done with correct url
+  getWiki();
+});
+
+/*
 function fromSearchBox () {
   var frm = document.getElementById('inp');
   var userInput = frm.value;
@@ -30,5 +50,6 @@ $('.inp').on('click', function () {
   var urlForAjax;
   fromSearchBox();
 });
+*/
 
 getWiki();
