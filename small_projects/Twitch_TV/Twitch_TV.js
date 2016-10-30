2bdedf55acf4fd290976aaf0951a81ceacb044c7
GeniaT <tchistiakov.e@gmail.com>
2016-10-30 21:59:21 +0100

    html feed + css setting for global look

--- small_projects/Twitch_TV/Twitch_TV.js
+++ small_projects/Twitch_TV/Twitch_TV.js
@@ -2,15 +2,17 @@ var games = ["CS","LoL","GTA V", "HeartStone","FreeCodeCamp", "Dota2","OverWatch
 var gamePics = ["http://findicons.com/files/icons/1008/quiet/128/counterstrike.png","http://orig11.deviantart.net/8078/f/2013/336/5/d/league_of_legends_icon_by_theman4556-d6wic5y.png",
 "http://cdn.wccftech.com/wp-content/uploads/2012/03/GTA-V-Logo.png","http://iosicongallery.com/img/512/hearthstone-heroes-warcraft-2015.png","https://avatars2.githubusercontent.com/u/9892522?v=3&s=400",
 "http://orig15.deviantart.net/3881/f/2011/230/b/b/dota_2_game_icon_by_wolfangraul-d4705v9.png","https://pbs.twimg.com/profile_images/633856419490480128/58pBUIoE_400x400.png",
-"http://static1.gamespot.com/uploads/scale_medium/1365/13658182/3077168-titanfall2icon.jpg"];
-var urls = ["https://api.twitch.tv/kraken/streams/amazhs","https://api.twitch.tv/kraken/streams/underground_dv","https://api.twitch.tv/kraken/streams/proleaguecsgo",
-"https://api.twitch.tv/kraken/streams/pandagirl91","https://api.twitch.tv/kraken/streams/freecodecamp"];
+"http://static1.gamespot.com/uploads/scale_medium/1365/13658182/3077168-titanfall2icon.jpg","http://i.istockimg.com/file_thumbview_approve/21787271/5/stock-photo-21787271-error-404-or-page-not-found-sticky-warning.jpg"];
+var apiStreamCall = ["https://api.twitch.tv/kraken/streams/proleaguecsgo","https://api.twitch.tv/kraken/streams/imaqtpie","https://api.twitch.tv/kraken/streams/kevinho90",
+"https://api.twitch.tv/kraken/streams/playhearthstone","https://api.twitch.tv/kraken/streams/freecodecamp","https://api.twitch.tv/kraken/streams/dota2ruhub",
+"https://api.twitch.tv/kraken/streams/blizzard","https://api.twitch.tv/kraken/streams/cohhcarnage","https://api.twitch.tv/kraken/streams/comster404"];
 
-var channels = ["amazhs","underground_dv","proleaguecsgo","pandagirl91","freecodecamp","brunofin","comster404"];
+var channels = ["proleaguecsgo","imaqtpie","kevinho90","playhearthstone","freecodecamp","dota2ruhub","blizzard","cohhcarnage","comster404"];
 
 $.ajax({
  type: 'GET',
  //The url for various stream check is this format: url: 'https://api.twitch.tv/kraken/streams/ + "end of a stream visitor url" like "esl_LoL" from https://www.twitch.tv/esl_lol
+ url: apiStreamCall[8], //Once the function works, create a loop to go through the urls array.
  headers: {
    'Client-ID': 'f8xpjswqdd4dvy82cqqpxgxhohjt0wx'                               //Key that you get registering your app on twitch.
  },
@@ -18,7 +20,6 @@ $.ajax({
    var status;
    console.log(data);
    console.log(data.stream);
-
    if (data.stream === null) { //Check if channel online or not
      var status = "Offline";
      console.log("This channel is offline at the moment");
@@ -28,6 +29,8 @@ $.ajax({
      console.log("Lucky you, it's online!");
      console.log(status);
    }
-
+ },
+ error: function () {
+   console.log("This streamer does not exist");
  }
 });
