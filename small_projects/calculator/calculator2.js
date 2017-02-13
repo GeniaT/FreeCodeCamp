bcd4974151709a7ed1ed0b3a4d51040ca19fbfc0
Genia <gtchistiakov@gmail.com>
2017-02-13 07:32:42 +0100

    some fix ok but no calcul yet

--- small_projects/calculator/calculator2.js
+++ small_projects/calculator/calculator2.js
@@ -8,52 +8,83 @@ var secondLineChars = "";
 
 function inputControl (key) {
 
-    if ((Number(key) >= 0 && Number(key) <= 9)) { //
-      if (userInput.length < 8 && arrForCalcul[arrForCalcul.length-1] !== "%") {       //limit for the screen and no dots yet
-        userInput.push(key);
-        displayFirstLine(key);
-      }
+    if ((Number(key) >= 0 && Number(key) <= 9)) {
+          if ((userInput.length < 8) && (arrForCalcul[arrForCalcul.length-1] !== "%")) {       //limit for the screen and no dots yet
+            if ((Number(key) === 0) && (userInput.length === 0)) { //Avoid displaying multiple zeros on the screen
+              return;
+            } else if ((Number(key) === 0) && (userInput[userInput.length - 1]) === "%") {
+              return;
+            } else {
+              userInput.push(key);
+              displayFirstLine(key);
+              displaySecondLine(key);
+            }
+          }
 
     } else if ((key === ".") && (userInput.indexOf(key) === (-1))) {
-      userInput.push(key);
-      displayFirstLine(key);
-
+      if (userInput.length === 0) { //avoiding dots after % like 4%. resulting in a NaN later
+        return;
+      } else {
+        userInput.push(key);
+        displayFirstLine(key);
+        displaySecondLine(key);
+      }
     } else if (key === "/" || key === "x" || key === "+" || key === "-") {
-      firstLineChars = ""; //we repplace the 1st line with the operational sign
-      if (userInput.length > 0) { // I only push into another arr if userInput isnt empty
-        arrForCalcul.push(Number(userInput.join(""))); //we push as a number the multi digit in the arr for later calculation
-      }
-      if ((typeof arrForCalcul[arrForCalcul.length-1] === "number") || (arrForCalcul[arrForCalcul.length-1] === "%")) {
-        arrForCalcul.push(key);
-        userInput = [];           //clearing the userInput so we can build a new string of digits
-      }
-      displayFirstLine(key);
-      if (firstLineChars.length > 0) { //after the displayFirstLine fct run, we clear the v
-        firstLineChars = "";
-      }
+          firstLineChars = ""; //we repplace the 1st line with the operational sign
+          if (userInput.length > 0) { // I only push into another arr if userInput isnt empty
+            arrForCalcul.push(Number(userInput.join(""))); //we push as a number the multi digit in the arr for later calculation
+          }
+          if ((typeof arrForCalcul[arrForCalcul.length-1] === "number") || (arrForCalcul[arrForCalcul.length-1] === "%")) {
+            arrForCalcul.push(key);
+            userInput = [];           //clearing the userInput so we can build a new string of digits
+          }
+          displayFirstLine(key);
+          displaySecondLine(key);
+          if (firstLineChars.length > 0) { //after the displayFirstLine fct run, we clear the variable
+            firstLineChars = "";
+          }
+
     } else if ( key === "%") {
-      if (userInput.length > 0) {
-        arrForCalcul.push(Number(userInput.join("")));
-        userInput = [];
-      }
-      if (typeof arrForCalcul[arrForCalcul.length-1] === "number") {
-        arrForCalcul.push(key);
-      }
+          if (userInput.length > 0) {
+            arrForCalcul.push(Number(userInput.join("")));
+            userInput = [];
+          }
+          if (typeof arrForCalcul[arrForCalcul.length-1] === "number") {
+            arrForCalcul.push(key);
+            displayFirstLine(key);
+            displaySecondLine(key);
+          }
     } else if (key === "C") {
-      clear();
+          clear();
     }
 
 }
 
-function operations(nbr1, nbr2) {
-   function add(nbr1, nbr2) {
-      var result = (Math.round(nbr1 + nbr2) * 1000) / 1000;
-  }
+
+function add(nbr1, nbr2) {
+  var result = Math.round((nbr1 + nbr2) * 1000) / 1000;
 }
+
+function sub(nbr1, nbr2) {
+  var result = Math.round((nbr1 - nbr2) * 1000) / 1000;
+}
+
+function mul(nbr1, nbr2) {
+  var result = Math.round((nbr1 * nbr2) * 1000) / 1000;
+}
+
+function div(nbr1, nbr2) {
+  var result = Math.round((nbr1 / nbr2) * 1000) / 1000;
+}
+
 
 function clear () {
   userInput = [];
   arrForCalcul = [];
+  firstLineChars = "";
+  secondLineChars = "";
+  $(".lineOne").html("0");
+  $(".lineTwo").html("0");
 }
 
   function displayFirstLine(key) {
@@ -61,13 +92,22 @@ function clear () {
     $(".lineOne").html(firstLineChars);
   }
 
-  function displaySecondLine() {
-
+  function displaySecondLine(key) {
+    var secondLine = arrForCalcul.join("");
+    if (arrForCalcul.length > 0) {
+      $(".lineTwo").html(secondLine);
+    }
   }
 
 
 function calculateResult() {
+  //reduce the array of chained operations by calculating first the %
 
+  //Then the * and /
+
+  //then + and -
+
+  //return a final value
 }
 
 
@@ -75,4 +115,9 @@ $(".key").click(function () {
   inputControl(this.innerHTML);
   console.log(userInput);
   console.log(arrForCalcul);
+  console.log(firstLineChars);
+});
+
+$("#calculate").click(function () {
+  calculateResult();
 });
