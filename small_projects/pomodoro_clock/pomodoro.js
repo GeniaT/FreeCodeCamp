24e4703d7d42c1030bdb606324d1d8ef2e4ece93
Genia <gtchistiakov@gmail.com>
2017-03-19 17:27:00 +0100

    start/pause logic setting
    done, next: timer update

--- small_projects/pomodoro_clock/pomodoro.js
+++ small_projects/pomodoro_clock/pomodoro.js
@@ -1,14 +1,60 @@
-//events creation vars
-var eventStart
-var eventPause
-var eventResetTimer
-var eventPomodoroChoosen
+var start; //will be passed on "on" or "off" when clicking on start/pause button
+var countdown;
+//Setting the timer according to the choice of Pomodoro duration
+function updateTimerAtInit() {
+  var min = $('#pomodoro_duration').val();
+  var sec = "00";
+  var timerValue;
+
+  if (min > 5) { //Display 05:00 instead of 5:00
+    timerValue = min + " : " + sec;
+  } else {
+    timerValue = "0" + min + " : " + sec;
+  }
+
+  $('.timer').html(timerValue);
+}
+
+function countDown() {
+  console.log("one sec has passed");
+}
 
 
-$('.reset').on('click', function() {
-  console.log('A reset should now happen!');
+ //takes the time by default
+ updateTimerAtInit();
+//Functions run based on different events
+$('#pomodoro_duration').on('change', function() {
+  updateTimerAtInit();
 });
 
-$('.pomodoro_duration').on('change', function() {
-  console.log('the duration has changed!');
-})
+$('.reset').on('click', function() {
+  $('#pomodoro_duration').val("25");//before updating the timer, we reset the option selected as default in pom duration
+  $('#break_duration').val("5");
+  updateTimerAtInit();
+});
+
+$('.start_pause').on('click', function() { //Depending on if the timer state (running or in pause), we start/pause it and change it's state again.
+  var timer = $('.timer').html();
+  if (start === "on") {
+    console.log("PAUSED!");
+    clearInterval(countdown);
+    start = "off";
+  } else {
+    countdown = setInterval(countDown, 1000);
+    console.log("started!");
+    start = "on";
+  }
+
+
+
+  //check if the function that will modify the timer is ready
+
+  //create a setInterval with this function each sec
+
+  //test if ok.
+});
+/*
+Logics to add:
+  updateTimer is run at reset, pause, end of pomodoro, end of break, at the init of the app.
+  Need to differenciate the cases so the update can be ran according to the trigger.
+*/
